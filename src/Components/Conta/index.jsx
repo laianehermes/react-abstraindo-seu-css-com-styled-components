3fdacae4e60c1638185dd26de8a71eefc8dce5bd
lsouza <lsouza@luxfacta.com>
2021-07-13 16:46:16 -0300

    apply theme in project

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
-import { Icone, Box, Botao, Saldo, Detalhe } from "../UI";
+import { Icone, Box, Botao, Saldo, Detalhe, IconeTema } from "../UI";
 import styled from "styled-components";
 
 const IconeMargin = styled(Icone)`
@@ -23,7 +23,7 @@ const Conta = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <Icone src={dinheiro} alt="Ícone Saldo" />
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
           <Saldo>
--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -6,8 +6,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: "Montserrat", sans-serif;
     margin: 0;
     padding: 0;
-    text-decoration: none;
-    color: grey;
+    text-decoration: none;    
   }
 `;
 
